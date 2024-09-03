8f1d674a8cce64d58fbfa8a7ea468e1e9349b215
ygor caina <ygorcaina2018@gmail.com>
2024-09-03 08:54:43 -0300

    trabalhando com comandos de entrada e saída em paython

---
+++ exemplo1.py
@@ -0,0 +1,2 @@
+print("Olá mundo")
+print("Progamando em python\n")
---
+++ exemplo2.py
@@ -0,0 +1,5 @@
+nome = input("Informe seu nome: ")
+end = input("Informe seu endereço: ")
+idade = input("Informe sua idade: ")
+
+print(nome, end, idade)
