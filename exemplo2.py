cfbd5f32338fd625ea3e7c6673529562a34f1967
ygor caina <ygorcaina2018@gmail.com>
2024-09-03 09:49:09 -0300

    cauculando média com python

--- exemplo2.py
+++ exemplo2.py
@@ -2,4 +2,14 @@ nome = input("Informe seu nome: ")
 end = input("Informe seu endereço: ")
 idade = input("Informe sua idade: ")
 
-print(nome, end, idade)
+#Exibindo dados das variáveis
+#print(nome, end, idade)
+
+#1ª forma de concatenação
+print("\nOlá", nome, " seu endereço é", end," sua idade é", idade)
+
+#2ª forma de concatenação
+print("\nOlá {} seu endereço é {} e sua idade é {}". format(nome, end, idade))
+
+#3ª forma de concatenação - f string
+print(f"Bem vindo {nome}, voce mora na {end}, e tem {idade} anos")
---
+++ exemplo3.py
@@ -0,0 +1,7 @@
+nota1 = float (input("Informe sua 1ª nota: ")) #estamos convertendo os valores textuais em valores decimais.
+nota2 = float (input("Informe sua 2ª nota: "))
+nota3 = float (input("Informe sua 3ª nota: "))
+
+media = (nota1 + nota2 + nota3)/3
+
+print(f"Sua média é {media: .2f}")
